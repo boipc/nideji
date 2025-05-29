b4562a0b7f3f25e42242e00ef4842416f60f34cd
boipc <96824796+boipc@users.noreply.github.com>
2025-05-29 22:14:14 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,70 +1,21 @@
-// 缓存名称
+// sw.js
 const CACHE_NAME = 'salary-calculator-v1';
-
-// 需要缓存的资源
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  // 如果有其他资源（如CSS、JS、图片），也在这里添加
 ];
 
-// 安装事件
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('缓存已打开');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// 激活事件
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// 获取请求
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // 如果缓存中有，直接返回缓存
-        if (response) {
-          return response;
-        }
-        
-        // 否则从网络获取
-        return fetch(event.request).then(
-          response => {
-            // 检查响应是否有效
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // 克隆响应
-            const responseToCache = response.clone();
-
-            // 将响应添加到缓存
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
+    caches.match(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
